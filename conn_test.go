4046b52c20f08db6df5cd46863b76aa0c1c6a4b5
Command <maoxs2@gmail.com>
2020-05-30 15:06:16 +0800

    optimize reader

--- conn_test.go
+++ conn_test.go
@@ -12,10 +12,10 @@ import (
 	stream "github.com/maoxs2/go-aead-iostream"
 )
 
-func TestCryptoConnStream(t *testing.T) {
+func TestCryptoConnStreamCome(t *testing.T) {
 	seed := hash([]byte("Hello"))
 	rawMessage := []byte("Package cipher implements standard block cipher modes that can be wrapped around low-level block cipher implementations. See https://csrc.nist.gov/groups/ST/toolkit/BCM/current_modes.html and NIST Special Publication 800-38A.")
-	chunkSize := 64
+	chunkSize := 256
 
 	c1, err := aes.NewCipher(seed)
 	if err != nil {
@@ -39,7 +39,7 @@ func TestCryptoConnStream(t *testing.T) {
 	passCh := make(chan struct{})
 
 	// WRITE
-	l, err := net.Listen("tcp", "127.0.0.1:65535")
+	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		panic(err)
 	}
@@ -90,6 +90,93 @@ func TestCryptoConnStream(t *testing.T) {
 		}
 
 		passCh <- struct{}{}
+	}()
+
+	select {
+	case <-passCh:
+		break
+	}
+}
+
+func TestCryptoConnStreamTo(t *testing.T) {
+	seed := hash([]byte("Hello"))
+	rawMessage := []byte("Package cipher implements standard block cipher modes that can be wrapped around low-level block cipher implementations. See https://csrc.nist.gov/groups/ST/toolkit/BCM/current_modes.html and NIST Special Publication 800-38A.")
+	chunkSize := 64
+
+	c1, err := aes.NewCipher(seed)
+	if err != nil {
+		panic(err)
+	}
+
+	c2, err := aes.NewCipher(seed)
+	if err != nil {
+		panic(err)
+	}
+
+	aead1, err := cipher.NewGCM(c1)
+	if err != nil {
+		panic(err)
+	}
+	aead2, err := cipher.NewGCM(c2)
+	if err != nil {
+		panic(err)
+	}
+
+	passCh := make(chan struct{})
+
+	// WRITE
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				panic(err)
+			}
+
+			w := stream.NewCryptoConn(seed, chunkSize, conn, aead1)
+			buf := make([]byte, 2)
+			dst := make([]byte, 0)
+			for {
+				n, err := w.Read(buf)
+				if n > 0 {
+					dst = append(dst, buf[:n]...)
+				}
+				if err != nil && err != io.EOF {
+					panic(err)
+				}
+				if err == io.EOF {
+					break
+				}
+			}
+
+			if !bytes.Equal(dst, rawMessage) {
+				t.Errorf("dst is %s, but raw is %s", dst, rawMessage)
+			} else {
+				t.Log("pass")
+			}
+
+			passCh <- struct{}{}
+
+		}
+
+	}()
+
+	// READ
+	go func() {
+		time.Sleep(10)
+		conn, err := net.Dial("tcp", l.Addr().String())
+		if err != nil {
+			panic(err)
+		}
+
+		r := stream.NewCryptoConn(seed, chunkSize, conn, aead2)
+		r.Write(rawMessage)
+		r.Close()
 	}()
 
 	select {
--- reader.go
+++ reader.go
@@ -33,44 +33,47 @@ func NewStreamReader(seed []byte, chunkSize int, backend io.Reader, aead cipher.
 }
 
 func (r *StreamReader) Read(dst []byte) (n int, err error) {
+	if len(r.buf) > 0 {
+		n := copy(dst, r.buf)
+		r.buf = r.buf[n:]
+		return n, nil
+	}
+
+	nn, err := r.read()
+	n = copy(dst, r.buf[:nn])
+	r.buf = r.buf[n:]
+
+	return
+}
+
+func (r *StreamReader) read() (n int, err error) {
 	chunk := make([]byte, (2+r.chunkSize)+r.aead.Overhead())
 	fullChunSizeBytes := packUint16LE(uint16(r.chunkSize))
 
 	var l int
 
-	for {
-		l, err = r.r.Read(chunk)
+	l, err = io.ReadFull(r.r, chunk)
 
-		if err == io.ErrUnexpectedEOF {
-			chunk = chunk[:l]
-		}
+	if err != nil {
+		return 0, err
+	}
 
-		if err == io.EOF {
-			break
-		}
-
+	if l > 0 {
+		_, err = r.aead.Open(chunk[:0], r.seed, chunk, nil)
 		if err != nil {
+			fmt.Println(err)
 			return
 		}
 
-		if l > 0 {
-			_, err = r.aead.Open(chunk[:0], r.seed, chunk, nil)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-
-			if bytes.Equal(chunk[0:2], fullChunSizeBytes) {
-				r.buf = append(r.buf, chunk[2:2+r.chunkSize]...)
-			} else {
-				r.buf = append(r.buf, chunk[2:2+binary.LittleEndian.Uint16(chunk[0:2])]...)
-			}
-
+		if bytes.Equal(chunk[0:2], fullChunSizeBytes) {
+			n += r.chunkSize
+			r.buf = append(r.buf, chunk[2:2+r.chunkSize]...)
+		} else {
+			ll := binary.LittleEndian.Uint16(chunk[0:2])
+			n += int(ll)
+			r.buf = append(r.buf, chunk[2:2+ll]...)
 		}
 	}
-
-	n = copy(dst, r.buf)
-	r.buf = r.buf[n:]
 
 	return
 }
